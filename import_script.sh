9fec8f55aba7ab8776e69151d563d62f1183621f
Zbynek Vyhlas <zbyneksmail@gmail.com>
2024-06-27 12:23:50 +0200

    python to python3 in import_script

--- import_script.sh
+++ import_script.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-python manage.py load_opinions_nfs data/opinions.csv
+python3 manage.py load_opinions_nfs data/opinions.csv
 echo "Opinions loaded."
-python manage.py load_genotox data/genotox_studies.csv
+python3 manage.py load_genotox data/genotox_studies.csv
 echo "Genotox loaded."
-python manage.py load_adme data/adme.csv
+python3 manage.py load_adme data/adme.csv
 echo "ADME loaded."
-python manage.py load_endpoint_studies data/endpoint_studies.csv
+python3 manage.py load_endpoint_studies data/endpoint_studies.csv
 echo "Endpoint studies loaded."
-python manage.py load_chemical_identities data/chemical_identities.csv
+python3 manage.py load_chemical_identities data/chemical_identities.csv
 echo "Chemical identities loaded."
-python manage.py load_organism_identities data/organism_identities.csv
+python3 manage.py load_organism_identities data/organism_identities.csv
 echo "Organism identities loaded."
-python manage.py load_nf_variants data/use.csv
+python3 manage.py load_nf_variants data/use.csv
 echo "NF variants loaded."
-python manage.py load_processes data/processes.csv
+python3 manage.py load_processes data/processes.csv
 echo "Processes loaded."
-python manage.py load_composition data/composition.csv
+python3 manage.py load_composition data/composition.csv
 echo "Composition loaded."
