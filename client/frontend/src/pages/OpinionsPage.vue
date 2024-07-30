be85ef5cf271e558444b1b9cd469e7565a30a3c9
Zbynek Vyhlas <zbyneksmail@gmail.com>
2024-07-30 17:51:32 +0200

    new design for search demonstrated on Opinion

--- client/frontend/src/pages/OpinionsPage.vue
+++ client/frontend/src/pages/OpinionsPage.vue
@@ -2,23 +2,86 @@
   <div>
     <h1>Opinions</h1>
   </div>
+  <v-div>
+    <v-container>
+      <v-row class="mt-5">
+        <v-col cols="9">
+          <v-row>
+            <v-combobox
+              clearable
+              chips
+              multiple
+              label="Display in columns"
+              v-model="showInColumns"
+              :items="[
+                'Allergenicity',
+                'NF Code',
+                'Novel Food Title',
+                'Was Tox Study Required',
+                'Has Nutri Disadvantage',
+                'Opinion URL',
+                'Opinion doi',
+                'Publication date'
+              ]"
+            ></v-combobox>
+          </v-row>
+          <v-row>
+            <v-combobox
+              clearable
+              chips
+              multiple
+              v-model="showInDetails"
+              label="Display in details"
+              :items="[
+                'ADME Studies',
+                'Endpoint Studies',
+                'Genotox Studies',
+                'Final Outcomes',
+                'Final Outcome Remarks'
+              ]"
+            ></v-combobox>
+          </v-row>
+        </v-col>
+        <v-col cols="3">
+          <v-row class="justify-center">
+            <v-btn style="min-height: 50px" color="primary">
+              <v-icon left>mdi-export</v-icon>
+              Export
+            </v-btn>
+          </v-row>
+          <v-row class="mt-8 justify-center">
+            <v-btn style="min-height: 50px" color="secondary">
+              <v-icon left>mdi-magnify</v-icon>
+              Perform Search
+            </v-btn>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-div>
 
-  <v-row class="ma-3 justify-center">
-    <v-text-field
-      v-model="search"
-      clearable
-      density="comfortable"
-      hide-details
-      placeholder="Novel Food title"
-      prepend-inner-icon="mdi-magnify"
-      style="max-width: 300px"
-      variant="solo"
-    ></v-text-field>
-  </v-row>
-
-  <div>
-    <v-data-iterator item-value="id" :items="novelFoods" :items-per-page="4" :search="search">
-      <template v-slot:default="{ items, isExpanded, toggleExpand }">
+  <div class="mt-8">
+    <v-data-iterator item-value="id" :items="novelFoods" :items-per-page="8" :search="searchFilter">
+      <template v-slot:header>
+        <v-toolbar class="px-2" style="background-color: transparent">
+          <v-text-field
+            v-model="searchFilter"
+            density="comfortable"
+            placeholder="Search chosen fileters"
+            prepend-inner-icon="mdi-magnify"
+            style="max-width: 300px"
+            variant="solo"
+            clearable
+            hide-details
+          ></v-text-field>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" variant="outlined">
+            <v-icon left>mdi-plus</v-icon>
+            Add Filter
+          </v-btn>
+        </v-toolbar>
+      </template>
+      <template v-slot:default="{ items }">
         <v-row>
           <v-col v-for="(item, i) in items" :key="i" cols="12" md="6">
             <v-card border>
@@ -31,48 +94,36 @@
               </v-card-text>
 
               <v-table density="compact" class="text-caption">
+                <thead>
+                  <tr>
+                    <th class="text-left">Include / Exclude</th>
+                    <th class="text-left">Filter</th>
+                    <th class="text-left">Qualifier</th>
+                    <th class="text-left">Value</th>
+                  </tr>
+                </thead>
                 <tbody>
-                  <tr align="right">
-                    <th>NF Code:</th>
-                    <td>${{ item.raw.NFcode }}</td>
-                  </tr>
-                  <tr align="right">
-                    <th>DOI:</th>
-                    <td>{{ item.raw.doi }}</td>
-                  </tr>
-                  <tr align="right">
-                    <th>Question number:</th>
-                    <td>{{ item.raw.questionNo }}</td>
-                  </tr>
-                  <tr align="right">
-                    <th>Date of publication:</th>
-                    <td>{{ item.raw.published }}</td>
+                  <tr>
+                    <td>
+                      <b>{{ item.raw.include ? 'include' : 'exclude' }}</b>
+                    </td>
+                    <td>
+                      <b>{{ item.raw.title }}</b>
+                    </td>
+                    <td>
+                      <b>{{ item.raw.qualifier }}</b>
+                    </td>
+                    <td>
+                      <b>{{ item.raw.value }}</b>
+                    </td>
                   </tr>
                 </tbody>
               </v-table>
-              <div class="px-4">
-                <v-switch
-                  :model-value="isExpanded(item)"
-                  :label="`${isExpanded(item) ? 'Hide' : 'Show'} details`"
-                  density="compact"
-                  inset
-                  @click="() => toggleExpand(item)"
-                ></v-switch>
-                <v-divider></v-divider>
-                <v-expand-transition>
-                  <div v-if="isExpanded(item)">
-                    <v-card-text>
-                      A lot more information about the {{ item.raw.title }}: Lorem ipsum dolor sit
-                      amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                      et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                      in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum.
-                    </v-card-text>
-                  </div>
-                </v-expand-transition>
-              </div>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="tertiary" text>Remove</v-btn>
+              </v-card-actions>
             </v-card>
           </v-col>
         </v-row>
@@ -107,71 +158,68 @@
 <script>
 export default {
   data: () => ({
-    search: '',
+    searchFilter: '',
     expandedItems: {},
+    showInColumns: ['NF Code'],
+    showInDetails: [],
     novelFoods: [
       {
         id: 1,
-        NFcode: 'ad183d1-4732',
-        title: 'yellow/orange tomato extract',
-        subtitle: 'some subtitle about the yellow/orange tomato extract',
-        doi: 'https://doi.org/10.2903/j.efsa.2023.77894',
-        questionNo: 'EFSA‐Q‐2021‐001',
-        published: '2023/08/06'
+        include: true,
+        title: 'production process',
+        subtitle: 'production process',
+        qualifier: 'contains',
+        value: 'solvent steps'
       },
       {
         id: 2,
-        NFcode: '18a31-4732',
-        title: 'Isomalto-oligosaccharide',
-        subtitle: 'some subtitle about the Isomalto-oligosaccharide',
-        doi: 'https://doi.org/10.2903/j.efsa.2023.787294',
-        questionNo: 'EFSA‐Q‐2018‐00201',
-        published: '2023/12/07'
+        include: false,
+        title: 'opinion regulation',
+        subtitle: 'administrative',
+        qualifier: 'is',
+        value: 'Regulation (EC) No 2015/2283 Article 10'
       },
       {
         id: 3,
-        NFcode: '5adf-818',
-        title: 'Isomalto-oligosaccharide',
-        subtitle: 'some subtitle about the Isomalto-oligosaccharide',
-        doi: 'https://doi.org/10.2903/j.efsa.2021.8989',
-        questionNo: 'EFSA‐Q‐2021‐18601',
-        published: '2021/12/07'
+        include: true,
+
+        title: 'date of publication',
+        subtitle: 'administrative',
+        qualifier: 'greater than',
+        value: '01.07.2018'
       },
       {
         id: 4,
-        NFcode: 'a1df83df1-4fds732',
-        title: '2’-Fucosyllactose (2’-FL)',
-        subtitle: 'some subtitle about the 2’-Fucosyllactose (2’-FL)',
-        doi: 'https://doi.org/10.2903/j.efsa.2023.728794',
-        questionNo: 'EFSA‐Q‐2021‐7201',
-        published: '2023/03/06'
+        include: false,
+        title: 'type mandate',
+        subtitle: 'administrative',
+        qualifier: 'is',
+        value: 'extension of use'
       },
       {
         id: 5,
-        NFcode: '1448sf31-47f3s2',
-        title: 'Pelargonium sidoides',
-        subtitle: 'some subtitle about the Pelargonium sidoides',
-        doi: 'https://doi.org/10.2903/j.efsa.2023.28794',
-        questionNo: 'EFSA‐Q‐2022‐40201',
-        published: '2023/10/16'
+        include: true,
+        title: 'type mandate',
+        subtitle: 'administrative',
+        qualifier: 'is',
+        value: 'nutrient source'
       },
       {
         id: 6,
-        NFcode: '14411-4732',
-        title: 'Ashitaba sap powder',
-        subtitle: 'some subtitle about the Ashitaba sap powder',
-        doi: 'https://doi.org/10.2903/j.efsa.2023.72794',
-        questionNo: 'EFSA‐Q‐2031‐88201',
-        published: '2023/01/06'
+        include: true,
+        title: 'production process',
+        subtitle: 'production process',
+        qualifier: 'contains',
+        value: 'microorganism'
+
       },
       {
         id: 7,
-        NFcode: '8481-4732',
-        title: 'Synthetic paraxanthine',
-        subtitle: 'some subtitle about the Synthetic paraxanthine',
-        doi: 'https://doi.org/10.2903/j.efsa.2023.989794',
-        questionNo: 'EFSA‐Q‐2020‐70201',
-        published: '2023/10/02'
+        include: false,
+        title: 'production process',
+        subtitle: 'production process',
+        qualifier: 'contains',
+        value: 'enzymes'
       }
     ]
   })
