a346a95630ddb05d93f64f49677f33bf02256277
Zbynek Vyhlas <zbyneksmail@gmail.com>
2024-07-31 14:07:51 +0200

    basic frontend graphql setting

---
+++ client/frontend/src/libs/apollo-client.js
@@ -0,0 +1,59 @@
+// ApolloClient: This is the core class used to initiate a connection with a GraphQL server.
+// InMemoryCache: This is a standard caching implementation provided by Apollo. It stores query results in memory for better performance.
+// createHttpLink: This function helps create an HTTP link to connect your Apollo Client with your GraphQL server.
+// setContext: This function is used to set the context for each request, often used to add authentication headers.
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// Creates HTTP link to your GraphQL server
+// The URI is pointing to the GraphQL endpoint on the backend
+const httpLink = createHttpLink({
+  uri: 'http://127.0.0.1:8000/api/v1/graphql', //! The GraphQL endpoint. Replace with variable from .env file for better security and flexibility.
+});
+
+
+
+// Function to get the value of a cookie by name
+const getCookie = (name) => {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Check if this cookie string begins with the name we want
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+};
+
+// Set up the authentication link to include the JWT token from the cookie
+const authLink = setContext((_, { headers }) => {
+  // Retrieve the token from the 'access-token' cookie
+  const token = getCookie('access-token');
+
+  // Return the headers to the context so that HTTP link can read them
+  return {
+    headers: {
+      ...headers, // Spread the existing headers
+      authorization: token ? `Bearer ${token}` : "", // Add the Authorization header if the token exists
+    }
+  };
+});
+
+
+
+
+// Create an instance of Apollo Client
+const client = new ApolloClient({
+  // Combine the authentication link and the HTTP link
+  link: authLink.concat(httpLink), // First use authLink to set headers, then httpLink to handle the HTTP connection
+  // Use in-memory cache for better performance
+  cache: new InMemoryCache() // Apollo's standard caching mechanism to store query results in memory
+});
+
+// Exports the configured Apollo Client instance so it can be imported and used the Vue.js application
+export default client;
--- client/frontend/src/main.js
+++ client/frontend/src/main.js
@@ -1,14 +1,31 @@
 import '@/assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import App from '@/App.vue'
 import { setupStore } from '@/stores'
 import router from '@/router'
 import vuetify from '@/plugins/vuetify'
+import { ApolloClients } from '@vue/apollo-composable'
+import apolloClient from '@/libs/apollo-client'
 
-const app = createApp(App)
+const app = createApp({
+  setup() {
+    provide(ApolloClients, {
+      defaultClient: apolloClient
+    })
+  },
+  render: () => h(App)
+  // function to render the main App component
+})
+
+// Setup store
 setupStore(app)
+
+// Use Vuetify
 app.use(vuetify)
+
+// Use Vue Router
 app.use(router)
 
+// Mount the app\
 app.mount('#app')
