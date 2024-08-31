fb09b449174e7b760d6bd84879812df3ecf16ee9
Zbynek Vyhlas <zbyneksmail@gmail.com>
2024-08-31 15:52:59 +0200

    adding description in available attrs

--- client/frontend/src/libs/available-attrs.js
+++ client/frontend/src/libs/available-attrs.js
@@ -4,121 +4,141 @@ export const availableAttrs = [
     text: 'NF code',
     model: 'novelFoods',
     field: 'nfCode',
-    icon: 'mdi-rice'
+    icon: 'mdi-rice',
+    description: 'description'
   },
   {
     text: 'novel food title',
     model: 'novelFoods',
     field: 'title',
-    icon: 'mdi-rice'
+    icon: 'mdi-rice',
+    description: 'description'
   },
   {
     text: 'opinion document type',
     model: 'novelFoods',
     field: 'opinionDocumentType',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
   {
     text: 'Toxicology Study Required',
     model: 'novelFoods',
     field: 'toxStudyRequired',
-    icon: 'mdi-flask-outline'
+    icon: 'mdi-flask-outline',
+    description: 'description'
   },
   {
     text: 'Genotoxicity Final Outcome',
     model: 'novelFoods',
     field: 'genotoxFinalOutcome',
-    icon: 'mdi-dna'
+    icon: 'mdi-dna',
+    description: 'description'
   },
   {
     text: 'Final Toxicology Remarks',
     model: 'novelFoods',
     field: 'finalToxicologyRemarks',
-    icon: 'mdi-comment-text-outline'
+    icon: 'mdi-comment-text-outline',
+    description: 'description'
   },
   {
     text: 'Protein Digestibility',
     model: 'novelFoods',
     field: 'proteinDigestibility',
-    icon: 'mdi-food-drumstick-outline'
+    icon: 'mdi-food-drumstick-outline',
+    description: 'description'
   },
   {
     text: 'Antinutritional Factors',
     model: 'novelFoods',
     field: 'antinutritionalFactors',
-    icon: 'mdi-alert-circle-outline'
+    icon: 'mdi-alert-circle-outline',
+    description: 'description'
   },
   {
     text: 'Nutritional Disadvantage',
     model: 'novelFoods',
     field: 'hasNutriDisadvantage',
-    icon: 'mdi-scale-balance'
+    icon: 'mdi-scale-balance',
+    description: 'description'
   },
   {
     text: 'Nutritional Disadvantage Explanation',
     model: 'novelFoods',
     field: 'nutriDisadvantageExplanation',
-    icon: 'mdi-text-box-outline'
+    icon: 'mdi-text-box-outline',
+    description: 'description'
   },
   {
     text: 'Sufficient Data',
     model: 'novelFoods',
     field: 'sufficientData',
-    icon: 'mdi-database-check'
+    icon: 'mdi-database-check',
+    description: 'description'
   },
   {
     text: 'Food Matrices',
     model: 'novelFoods',
     field: 'foodMatrices',
-    icon: 'mdi-grid'
+    icon: 'mdi-grid',
+    description: 'description'
   },
   {
     text: 'Instability Concerns',
     model: 'novelFoods',
     field: 'instabilityConcerns',
-    icon: 'mdi-alert-outline'
+    icon: 'mdi-alert-outline',
+    description: 'description'
   },
   {
     text: 'Shelf Life Value',
     model: 'novelFoods',
     field: 'shelflifeValue',
-    icon: 'mdi-clock-outline'
+    icon: 'mdi-clock-outline',
+    description: 'description'
   },
   {
     text: 'Shelf Life Unit',
     model: 'novelFoods',
     field: 'shelflifeUnit',
-    icon: 'mdi-timer-sand'
+    icon: 'mdi-timer-sand',
+    description: 'description'
   },
   {
     text: 'Endocrine Disruptive Properties',
     model: 'novelFoods',
     field: 'endocrineDisruptProp',
-    icon: 'mdi-alert-outline'
+    icon: 'mdi-alert-outline',
+    description: 'description'
   },
   {
     text: 'Outcome',
     model: 'novelFoods',
     field: 'outcome',
-    icon: 'mdi-check-circle-outline'
+    icon: 'mdi-check-circle-outline',
+    description: 'description'
   },
   {
     text: 'Outcome Remarks',
     model: 'novelFoods',
     field: 'outcomeRemarks',
-    icon: 'mdi-comment-text-outline'
+    icon: 'mdi-comment-text-outline',
+    description: 'description'
   },
   {
     text: 'Vocabulary ID',
     model: 'novelFoods',
     field: 'vocabId',
-    icon: 'mdi-book-open-outline'
+    icon: 'mdi-book-open-outline',
+    description: 'description'
   },
   {
     text: 'Allergenicity',
     model: 'allergenicities',
     field: 'title',
-    icon: 'mdi-alert-circle-outline'
+    icon: 'mdi-alert-circle-outline',
+    description: 'description'
   },
 
   // opinion
@@ -126,61 +146,71 @@ export const availableAttrs = [
     text: 'opinion document type',
     model: 'novelFoods',
     field: 'opinionDocumentType',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
   {
     text: 'opinion title',
     model: 'novelFoods',
     field: 'opinionTitle',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
   {
     text: 'opinion doi',
     model: 'novelFoods',
     field: 'opinionDoi',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
   {
     text: 'opinion url',
     model: 'novelFoods',
     field: 'opinionUrl',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
   {
     text: 'opinion publication date',
     model: 'novelFoods',
     field: 'opinionPublicationDate',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
   {
     text: 'opinion adoption date',
     model: 'novelFoods',
     field: 'opinionAdoptionDate',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
   {
     text: "opinion's panel title",
     model: 'panels',
     field: 'title',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
   {
     text: "opinion's scientific officer first name",
     model: 'sciOfficers',
     field: 'firstName',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
   {
     text: "opinion's scientific officer middle name",
     model: 'sciOfficers',
     field: 'middleName',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
   {
     text: "opinion's scientific officer last name",
     model: 'sciOfficers',
     field: 'lastName',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
 
   // question
@@ -188,13 +218,15 @@ export const availableAttrs = [
     text: 'question number',
     model: 'questions',
     field: 'number',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
   {
     text: 'applicant title',
     model: 'applicants',
     field: 'title',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
 
   // mandate
@@ -202,18 +234,21 @@ export const availableAttrs = [
     text: 'mandate type title',
     model: 'mandates',
     field: 'mandateTypeTitle',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
   {
     text: 'mandate type definition',
     model: 'mandates',
     field: 'mandateTypeDefinition',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   },
   {
     text: 'mandate regulation',
     model: 'mandates',
     field: 'regulation',
-    icon: 'mdi-file-document-outline'
+    icon: 'mdi-file-document-outline',
+    description: 'description'
   }
 ]
